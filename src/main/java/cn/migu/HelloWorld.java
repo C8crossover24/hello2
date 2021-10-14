f75393da05852fdff9d623cba0fa9a4f6cc763e3
chengmengqi <1600916704@qq.com>
2021-10-14 10:46:06 +0800

    first commit

---
+++ src/main/java/cn/migu/HelloWorld.java
@@ -0,0 +1,13 @@
+package cn.migu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        List<String> objects = new ArrayList<String>();
+        objects.add("咪咕视频");
+        objects.add("咪咕阅读");
+        System.out.println(objects);
+    }
+}
